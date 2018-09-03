6276f7ef2ed569784e46ebfebac15432e66d9ac0
Calteo <matthias@famger.de>
2018-09-03 22:48:06 +0200

    code cleaning

--- Imap.Toolkit.Core/Class1.cs
+++ Imap.Toolkit.Core/Class1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Imap.Toolkit.Core
+﻿namespace Imap.Toolkit.Core
 {
     /// <summary>
     /// Some Comment
