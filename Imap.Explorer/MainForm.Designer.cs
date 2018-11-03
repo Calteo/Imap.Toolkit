e391686e47e4bd69cf79a143c94a765aa142917c
Calteo <matthias@famger.de>
2018-11-03 17:41:06 +0100

    New application Explorer added

---
+++ Imap.Explorer/MainForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Imap.Explorer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripMain = new System.Windows.Forms.ToolStrip();
+            this.toolStripComboBoxProfiles = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripMain
+            // 
+            this.toolStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBoxProfiles});
+            this.toolStripMain.Location = new System.Drawing.Point(0, 0);
+            this.toolStripMain.Name = "toolStripMain";
+            this.toolStripMain.Size = new System.Drawing.Size(868, 25);
+            this.toolStripMain.Stretch = true;
+            this.toolStripMain.TabIndex = 0;
+            // 
+            // toolStripComboBoxProfiles
+            // 
+            this.toolStripComboBoxProfiles.Name = "toolStripComboBoxProfiles";
+            this.toolStripComboBoxProfiles.Size = new System.Drawing.Size(121, 25);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 485);
+            this.Controls.Add(this.toolStripMain);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.toolStripMain.ResumeLayout(false);
+            this.toolStripMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStripMain;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBoxProfiles;
+    }
+}
+
---
+++ Imap.Explorer/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Imap.Explorer")]
+[assembly: AssemblyDescription("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2a47414b-1ab7-4212-b22a-b48b2e909b17")]
